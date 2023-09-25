e982dbdfc14145720a2a55f4cc87b06b810e270d
DanielArce2901 <arcedaniel2901@gmail.com>
2023-09-25 16:22:39 -0600

    menu

--- app.py
+++ app.py
@@ -1,24 +1,55 @@
 import streamlit as st
-from py2neo import Graph, Node, Relationship
-import pandas as pd
-from io import StringIO
-from neo4j import GraphDatabase
 from backend.main import procesar_archivos
-
 
 def main():
     st.title("Proyecto 1 Bases de datos Avanzadas")
-    uploaded_files = st.file_uploader("Subir archivos CSV", type=["csv"], accept_multiple_files=True)
     
-    if st.button("Cargar Datos"):
-        if uploaded_files:
-            procesar_archivos(uploaded_files)  # Cargar nodos primero
-            #crear_relaciones(uploaded_files)  # Luego crear relaciones
-            st.success("Los datos se han cargado correctamente en Neo4j.")
-        else:
-            st.warning("Por favor, suba los archivos CSV primero.")
-
-    # Agrega aquí las opciones para analizar las relaciones entre investigadores, proyectos y publicaciones.
-
+    # Crear el menú principal
+    menu = ["Consultas", "Subir Datos", "Gestión de Datos"]
+    choice = st.sidebar.selectbox("Menú", menu)
+    
+    if choice == "Consultas":
+        st.subheader("Sección de Consultas")
+        # Aquí puedes agregar el código para manejar las consultas.
+        
+    elif choice == "Subir Datos":
+        st.subheader("Sección para Subir Datos")
+        uploaded_files = st.file_uploader("Subir archivos CSV", type=["csv"], accept_multiple_files=True)
+        
+        if st.button("Cargar Datos"):
+            if uploaded_files:
+                procesar_archivos(uploaded_files)  # Cargar nodos primero
+                #crear_relaciones(uploaded_files)  # Luego crear relaciones
+                st.success("Los datos se han cargado correctamente en Neo4j.")
+            else:
+                st.warning("Por favor, suba los archivos CSV primero.")
+                
+    elif choice == "Gestión de Datos":
+        st.subheader("Sección de Gestión de Datos")
+        
+        # Crear el submenú para Gestión de Datos
+        gestion_menu = ["CRUD 1", "CRUD 2", "CRUD 3", "Asociar Investigador", "Asociar Artículo"]
+        gestion_choice = st.sidebar.selectbox("Gestión de Datos", gestion_menu)
+        
+        if gestion_choice == "CRUD 1":
+            st.subheader("CRUD 1")
+            # Aquí puedes agregar el código para manejar el CRUD 1.
+            
+        elif gestion_choice == "CRUD 2":
+            st.subheader("CRUD 2")
+            # Aquí puedes agregar el código para manejar el CRUD 2.
+            
+        elif gestion_choice == "CRUD 3":
+            st.subheader("CRUD 3")
+            # Aquí puedes agregar el código para manejar el CRUD 3.
+            
+        elif gestion_choice == "Asociar Investigador":
+            st.subheader("Asociar Investigador")
+            # Aquí puedes agregar el código para manejar la asociación de investigadores.
+            
+        elif gestion_choice == "Asociar Artículo":
+            st.subheader("Asociar Artículo")
+            # Aquí puedes agregar el código para manejar la asociación de artículos.
+            
 if __name__ == "__main__":
     main()
