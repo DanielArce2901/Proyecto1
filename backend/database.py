e292076586f7fb26a00599cb24f967ea496f2b94
DanielArce2901 <arcedaniel2901@gmail.com>
2023-09-27 00:52:49 -0600

    Asociar articulos e investigadores con proyectos

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,7 @@
 from neo4j import GraphDatabase
-from py2neo import Graph, Node
+from py2neo import Graph, Node, Relationship
+import numpy as np
+
 
 URI = "neo4j+s://1e46531c.databases.neo4j.io"
 AUTH = ("neo4j", "proyectoBases12")
@@ -10,3 +12,75 @@ def crear_nodos_en_neo4j(nodos):
         tipo = nodo.pop("tipo")
         node = Node(tipo, **nodo)
         graph.create(node)
+
+def crear_relaciones_para_Inv_Proy(relaciones):
+    graph = Graph(URI, auth=AUTH)
+    for relacion in relaciones:
+        try:
+            # Obtener y convertir los IDs a int
+            idInv = int(relacion.get("idInv", 0))
+            idProy = int(relacion.get("idProy", 0))
+            
+            # Verificar si los IDs son válidos antes de buscar los nodos
+            if idInv and idProy:
+                # Verificar si ambos nodos existen antes de crear la relación
+                if verificar_nodos_Inv_Proy(graph, idInv, idProy):
+                    investigador = graph.nodes.match("Investigadores", id=idInv).first()
+                    proyecto = graph.nodes.match("Proyectos", idPry=idProy).first()
+                    graph.create(Relationship(investigador, "PARTICIPA_EN", proyecto))
+                else:
+                    print(f"No se pudo crear la relación entre Investigador {idInv} y Proyecto {idProy} porque uno o ambos nodos no existen.")
+            else:
+                print(f"IDs inválidos: Investigador {idInv}, Proyecto {idProy}")
+        except Exception as e:
+            print(f"Error al crear la relación entre Investigador {relacion.get('idInv')} y Proyecto {relacion.get('idProy')}: {e}")
+
+
+
+def verificar_nodos_Inv_Proy(graph, idInv, idProy):
+    investigador = graph.nodes.match("Investigadores", id=idInv).first()
+    proyecto = graph.nodes.match("Proyectos", idPry=idProy).first()
+    
+    if investigador is None:
+        print(f"No se encontró el nodo Investigador con id={idInv}")
+    if proyecto is None:
+        print(f"No se encontró el nodo Proyecto con id={idProy}")
+    
+    return investigador is not None and proyecto is not None
+
+
+def crear_relaciones_para_publ_Proy(relaciones):
+    graph = Graph(URI, auth=AUTH)
+    for relacion in relaciones:
+        try:
+            # Obtener y convertir los IDs a int
+            idPubl = int(relacion.get("idArt", 0))
+            idProy = int(relacion.get("idProyecto", 0))
+            
+            # Verificar si los IDs son válidos antes de buscar los nodos
+            if idPubl and idProy:
+                # Verificar si ambos nodos existen antes de crear la relación
+                if verificar_nodos_Inv_Proy(graph, idPubl, idProy):
+                    publicacion = graph.nodes.match("Publicaciones", idPub=idPubl).first()
+                    proyecto = graph.nodes.match("Proyectos", idPry=idProy).first()
+                    graph.create(Relationship(publicacion, "PERTENECE_A", proyecto))
+                else:
+                    print(f"No se pudo crear la relación entre Publicación {idPubl} y Proyecto {idProy} porque uno o ambos nodos no existen.")
+            else:
+                print(f"IDs inválidos: Publicación {idPubl}, Proyecto {idProy}")
+        except Exception as e:
+            print(f"Error al crear la relación entre Publicación {relacion.get('idPubl')} y Proyecto {relacion.get('idProy')}: {e}")
+            
+def verificar_nodos_Plu_Proy(graph, idPubl, idProy):
+    publicacion = graph.nodes.match("Publicaciones", idPub=idPubl).first()
+    proyecto = graph.nodes.match("Proyectos", idPry=idProy).first()
+    
+    if publicacion is None:
+        print(f"No se encontró el nodo Publicación con id={idPubl}")
+    if proyecto is None:
+        print(f"No se encontró el nodo Proyecto con id={idProy}")
+    
+    return publicacion is not None and proyecto is not None
+
+
+
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from io import StringIO
 from backend.database import crear_nodos_en_neo4j
+from backend.database import crear_relaciones_para_Inv_Proy, crear_relaciones_para_publ_Proy
 
 
 def procesar_archivos(uploaded_files):
@@ -26,3 +27,54 @@ def procesar_archivos(uploaded_files):
                 nodos.append(nodo)
     crear_nodos_en_neo4j(nodos)
     return nodos
+
+
+def procesar_relaciones(uploaded_files):
+    relaciones = []
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            # Leer el contenido del archivo CSV
+            csv_content = uploaded_file.read().decode("utf-8")
+            
+            # Crear un archivo temporal en memoria a partir del contenido
+            csv_file = StringIO(csv_content)
+            
+            # Leer las relaciones del archivo CSV
+            df = pd.read_csv(csv_file, skipinitialspace=True)
+            for _, row in df.iterrows():
+                relacion = {
+                    "idInv": row["idInv"],
+                    "idProy": row["idProy"]
+                }
+                relaciones.append(relacion)
+    
+    # Llamar a la función para crear relaciones en Neo4j
+    crear_relaciones_para_Inv_Proy(relaciones)
+    return relaciones
+
+def procesar_relaciones_entre_publ_Proy(uploaded_files):
+    relaciones = []
+    for uploaded_file in uploaded_files:
+        if uploaded_file is not None:
+            # Leer el contenido del archivo CSV
+            csv_content = uploaded_file.read().decode("utf-8")
+            
+            # Crear un archivo temporal en memoria a partir del contenido
+            csv_file = StringIO(csv_content)
+            
+            # Leer las relaciones del archivo CSV
+            df = pd.read_csv(csv_file, skipinitialspace=True)
+            
+            # Imprimir los nombres de las columnas para verificar
+            print("Columnas del DataFrame:", df.columns)
+            
+            for _, row in df.iterrows():
+                relacion = {
+                    "idArt": row.get("idArt", None),  # Usar None como valor predeterminado
+                    "idProyecto": row.get("idProyecto", None)   # Usar None como valor predeterminado
+                }
+                relaciones.append(relacion)
+    
+    # Llamar a la función para crear relaciones en Neo4j
+    crear_relaciones_para_publ_Proy(relaciones)
+    return relaciones
